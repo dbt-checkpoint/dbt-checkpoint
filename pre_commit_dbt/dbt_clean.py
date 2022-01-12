06bca9e6714a364f4b3ad3b022674a8d1b6c6b64
Alex Ostojic <alex@montrealanalytics.com>
2022-01-12 16:47:17 -0500

    changed dbt_clean hook to support arguements

--- pre_commit_dbt/dbt_clean.py
+++ pre_commit_dbt/dbt_clean.py
@@ -1,17 +1,30 @@
+import argparse
 from typing import List
 from typing import Optional
 from typing import Sequence
 
+from pre_commit_dbt.utils import add_dbt_cmd_args
+from pre_commit_dbt.utils import get_flags
 from pre_commit_dbt.utils import run_dbt_cmd
 
 
-def prepare_cmd() -> List[str]:
-    cmd = ["dbt", "clean"]
+def prepare_cmd(
+    global_flags: Optional[Sequence[str]] = None,
+    cmd_flags: Optional[Sequence[str]] = None,
+) -> List[str]:
+    global_flags = get_flags(global_flags)
+    cmd_flags = get_flags(cmd_flags)
+    cmd = ["dbt", *global_flags, "clean", *cmd_flags]
     return cmd
 
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
-    cmd = prepare_cmd()
+    parser = argparse.ArgumentParser()
+    add_dbt_cmd_args(parser)
+
+    args = parser.parse_args(argv)
+
+    cmd = docs_generate_cmd(args.global_flags, args.cmd_flags)
     return run_dbt_cmd(cmd)
 
 
