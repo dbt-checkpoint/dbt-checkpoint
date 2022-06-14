fa6c676c0204bbf968963a1c60513a0ce871ddbb
Eve Johns <eve.johns@cityblock.com>
2022-06-14 15:05:36 -0700

    added an optional argument to fix the issue #36

--- pre_commit_dbt/check_script_has_no_table_name.py
+++ pre_commit_dbt/check_script_has_no_table_name.py
@@ -42,7 +42,7 @@ def add_space_to_parenthesis(sql: str) -> str:
 
 
 def has_table_name(
-    sql: str, filename: str, dotless: Optional[bool] = False
+    sql: str, filename: str, dotless: Optional[bool] = False, require2dots:Optional[bool] = False
 ) -> Tuple[int, Set[str]]:
     status_code = 0
     sql_clean = replace_comments(sql)
@@ -55,6 +55,8 @@ def has_table_name(
         if prev in ["from", "join"] and cur not in IGNORE_WORDS:
             table = cur.lower().strip().replace(",", "") if cur else cur
             if dotless and "." not in table:
+                pass
+            if require2dots and table.count(".")<2:
                 pass
             else:
                 tables.add(table)
@@ -74,6 +76,7 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     add_filenames_args(parser)
 
     parser.add_argument("--ignore-dotless-table", action="store_true")
+    parser.add_argument("--require-two-dots-table", action="store_true")
 
     args = parser.parse_args(argv)
     status_code = 0
@@ -81,7 +84,7 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     for filename in args.filenames:
         sql = Path(filename).read_text()
         status_code_file, tables = has_table_name(
-            sql, filename, args.ignore_dotless_table
+            sql, filename, args.ignore_dotless_table, args.require_two_dots_table
         )
         if status_code_file:
             result = "\n- ".join(list(tables))  # pragma: no mutate
@@ -96,3 +99,4 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
 
 if __name__ == "__main__":
     exit(main())
+
