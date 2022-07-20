9ba148b42bea3ce7f95e2f52b4c4e5292e1e25e3
Bruno Antonellini <antonellinibruno@gmail.com>
2022-07-20 12:10:58 -0300

    Utils function

--- pre_commit_dbt/check_model_has_description.py
+++ pre_commit_dbt/check_model_has_description.py
@@ -17,7 +17,7 @@ from pre_commit_dbt.utils import get_missing_file_paths
 def has_description(paths: Sequence[str], manifest: Dict[str, Any]) -> int:
     #print(paths)
     #print(manifest)
-    paths= get_missing_file_paths(paths, manifest)
+    #paths= get_missing_file_paths(paths, manifest)
     status_code = 0
     ymls = get_filenames(paths, [".yml", ".yaml"])
     sqls = get_model_sqls(paths, manifest)
@@ -25,6 +25,9 @@ def has_description(paths: Sequence[str], manifest: Dict[str, Any]) -> int:
 
     # get manifest nodes that pre-commit found as changed
     models = get_models(manifest, filenames)
+
+    print(models)
+
     # if user added schema but did not rerun the model
     schemas = get_model_schemas(list(ymls.values()), filenames)
     # convert to sets
