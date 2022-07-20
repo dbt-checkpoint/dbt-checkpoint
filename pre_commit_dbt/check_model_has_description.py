eaac525755e86e6ec945cae1236bb0bf5029a7f5
Bruno Antonellini <antonellinibruno@gmail.com>
2022-07-20 11:42:02 -0300

    Utils function

--- pre_commit_dbt/check_model_has_description.py
+++ pre_commit_dbt/check_model_has_description.py
@@ -15,7 +15,9 @@ from pre_commit_dbt.utils import JsonOpenError
 from pre_commit_dbt.utils import get_missing_file_paths
 
 def has_description(paths: Sequence[str], manifest: Dict[str, Any]) -> int:
-    paths= get_missing_file_paths(paths, manifest)
+    print(paths)
+    print(manifest)
+    #paths= get_missing_file_paths(paths, manifest)
     status_code = 0
     ymls = get_filenames(paths, [".yml", ".yaml"])
     sqls = get_model_sqls(paths, manifest)
