ade7cb2c5a928c0191ad22946ae4b1db394f2076
Prasanth Kommini <prasanth.kommini@snowflake.com>
2023-02-23 21:44:03 -0800

    Remove unnecessary variables.

--- pre_commit_dbt/utils.py
+++ pre_commit_dbt/utils.py
@@ -466,10 +466,10 @@ def add_related_ymls(
                 # Original patch_path has 'project\\path\to\yml.yml'
                 patch_path = Path(patch_path)
                 # Remove the project_name from patch_path
-                clean_patch_path = patch_path.relative_to(*patch_path.parts[:1])
+                target_yml_path = patch_path.relative_to(*patch_path.parts[:1])
 
-                if clean_patch_path not in paths_with_missing:
-                    paths_with_missing.append(clean_patch_path)
+                if target_yml_path not in paths_with_missing:
+                    paths_with_missing.append(target_yml_path)
 
 
 def get_missing_file_paths(
