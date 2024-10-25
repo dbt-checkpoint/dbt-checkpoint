8f41717833f2a8de96cb3a557964285d88eda0fc
Bruno Antonellini <antonellinibruno@gmail.com>
2024-10-25 14:55:30 -0300

    Feat-2974: ref-and-source support for Seeds and Snapshots (#266)
    * Feat-2974: ref-and-source support for Seeds and Snapshots

--- dbt_checkpoint/check_script_ref_and_source.py
+++ dbt_checkpoint/check_script_ref_and_source.py
@@ -16,69 +16,60 @@ from dbt_checkpoint.utils import (
 )
 
 
+def obj_exists_in_manifest(
+    obj_name: str,
+    manifest_sources: Dict[str, Any],
+    manifest_nodes: Dict[str, Any],
+    is_source: bool = False,
+) -> bool:
+    if is_source:
+        lookup_obj = manifest_sources
+    else:
+        lookup_obj = manifest_nodes
+    for _, value in lookup_obj.items():
+        lookup_name = value.get("unique_id")
+        if obj_name == lookup_name:
+            return True
+    return False
+
+
 def check_refs_sources(
     paths: Sequence[str], manifest: Dict[str, Any]
 ) -> Dict[str, Any]:
     status_code = 0
     sqls = get_filenames(paths, [".sql"])
-
+    manifest_sources = manifest.get("sources", {})
+    manifest_nodes = manifest.get("nodes", {})
     models = set()
     sources = {}
     for _, file in sqls.items():
-        full_script = file.read_text(encoding="utf-8")
-        full_script = replace_comments(full_script)
-        src_refs = re.findall(r"\{\{\s*(source|ref)\s*\((.*)\)\s*\}\}", full_script)
-        for src_ref in src_refs:
-            src_ref_value = src_ref[1].replace("'", "").replace('"', "").strip()
-            if src_ref[0] == "ref":
-                ref_node = get_manifest_node_from_file_path(manifest, str(file))
-                refs = ref_node.get("refs", [])
-                if not refs:
-                    models.add(src_ref_value)
-                for ref in refs:
-                    ref_id = f"model.{ref.get('package') or ref_node['package_name']}.{ref.get('name')}"
-                    if ref.get("version"):
-                        ref_id += f".v{ref.get('version')}"
-                    models.add(ref_id)
-            if src_ref[0] == "source":
-                src_split = src_ref_value.split(",")
-                source_name = src_split[0].strip()
-                table_name = src_split[1].strip()
-                src_key = frozenset([source_name, table_name])
-                sources[src_key] = {
-                    "source_name": source_name,
-                    "table_name": table_name,
-                }
-
-    if models:
-        nodes = manifest.get("nodes", {})
-        for _, value in nodes.items():
-            model_name = value.get("name")
-            model_id = value.get("unique_id")
-            if model_id in models:
-                models.remove(model_id)
-            elif model_name in models:
-                models.remove(model_name)
-
-    if sources:
-        srcs = manifest.get("sources", {})
-        for _, value in srcs.items():
-            source_set = frozenset([value.get("source_name"), value.get("name")])
-            if source_set in sources.keys():
-                sources.pop(source_set)
-
-    for _, src in sources.items():
-        status_code = 1
-        source_name = src.get("source_name")  # pragma: no mutate
-        table_name = src.get("table_name")  # pragma: no mutate
-        print(f"Missing source `{red(f'{source_name}.{table_name}')}`")
-
-    for missing_ref in models:
-        status_code = 1
-        print(f"Missing model (ref) {red(missing_ref)}")
+        filename = str(file)
+        ref_node = get_manifest_node_from_file_path(manifest, str(file))
+        dependent_nodes = ref_node.get("depends_on", {}).get("nodes", [])
+        for node in dependent_nodes:
+            node_split = node.split(".")
+            node_type = node_split[0]
+            dependency_exists = obj_exists_in_manifest(
+                node,
+                manifest_sources,
+                manifest_nodes,
+                is_source=node_type == "source",
+            )
+            if not dependency_exists:
+                status_code = 1
+                print(f"Missing {node_type} {red(node)} in {red(filename)}")
+                if node_type == "model":
+                    models.add(node)
+                if node_type == "source":
+                    source_name = node_split[-2]
+                    table_name = node_split[-1]
+                    src_key = frozenset([source_name, table_name])
+                    sources[src_key] = {
+                        "source_name": source_name,
+                        "table_name": table_name,
+                    }
 
     hook_properties = {"status_code": status_code, "models": models, "sources": sources}
-
     return hook_properties
 
 
